8d233c835f0819d22fce685aa9a65c432d3854e7
TajAlasfiyaa Ishag <tajalasfiyaa@gmail.com>
2023-12-01 20:51:17 +0200

    playing with chakra

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
+import { Box, Text } from "@chakra-ui/react";
+
 function App() {
-  return <>hh</>;
+  return (
+    <>
+      <Box h={"100vh"} bg="#151515">
+        <NavBar />
+      </Box>
+    </>
+  );
 }
 
+const NavBar = () => {
+  return (
+    <>
+      <Box
+        alignItems="center"
+        justifyContent="space-around"
+        display="flex"
+        w={"100vw"}
+        h={"40px"}
+        bg={"black"}
+      >
+        <Text m={3} color="white">
+          RAWG
+        </Text>
+        <Box w={"844px"} h={"35px"} borderRadius={20} bg="white"></Box>
+        <Box w={"224px"} h={"35px"} borderRadius={20} bg="white"></Box>
+      </Box>
+    </>
+  );
+};
 export default App;
