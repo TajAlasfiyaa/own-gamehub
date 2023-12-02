f881719ddac373a885a9a32f6a51012d935c041b
TajAlasfiyaa Ishag <tajalasfiyaa@gmail.com>
2023-12-02 09:06:12 +0200

    implemnt dark themse

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,43 @@
-import { Box, Text } from "@chakra-ui/react";
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  Grid,
+  GridItem,
+  Switch,
+  Text,
+} from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <Box h={"100vh"} bg="#151515">
-        <NavBar />
-      </Box>
+      <Grid
+        templateAreas={`"nav nav " " side main" `}
+        gridTemplateColumns={"150px 1fr"}
+        h="200px"
+        gap="1"
+        color="blackAlpha.700"
+        fontWeight="bold"
+      >
+        <GridItem bg={"black"} area={"nav"}>
+          <NavBar />
+        </GridItem>
+
+        <GridItem p={3} bg={""} area={"side"}></GridItem>
+        <GridItem p={3} area={"main"}>
+          <FormControl display="flex" alignItems="center">
+            <FormLabel htmlFor="email-alerts" mb="0">
+              Enable email alerts?
+            </FormLabel>
+            <Switch id="email-alerts" />
+          </FormControl>
+        </GridItem>
+      </Grid>
     </>
   );
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const NavBar = () => {
   return (
     <>
